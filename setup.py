00364be27f649aeb3c842a2bd60af653e7b03324
Max Timkovich <mtimkovich@gmail.com>
2020-03-26 13:06:29 -0700

    oops

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    README = f.read()
+
+setup(
+    name='auTO',
+    url='https://github.com/mtimkovich/auTO',
+    version='1.4.0',
+    author='Max Timkovich',
+    author_email='max@timkovi.ch',
+    license='MIT',
+    description='A Discord bot for TOing netplay tournaments',
+    long_description=README,
+    install_requires=[
+        'discord.py >= 1.3.2',
+        'pyyaml',
+    ],
+    setup_requires=['wheel'],
+    python_requires='>=3.7',
+    entry_points={'console_scripts': ['auTO=auTO.auTO:main']},
+)
