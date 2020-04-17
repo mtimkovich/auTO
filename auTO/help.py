6c13c98610f33891329ad01f9a775161c5c33053
Max Timkovich <mtimkovich@gmail.com>
2020-04-17 15:42:22 -0700

    refactor help docs

--- auTO/help.py
+++ auTO/help.py
@@ -1,42 +1,41 @@
+class HelpDoc(dict):
+    def __init__(self, brief: str, description='', usage=''):
+        if not description:
+            description = self.descriptify(brief)
+        super().__init__(
+            brief=brief,
+            description=description,
+            usage=usage
+        )
+
+    def descriptify(self, s):
+        return s[0].upper() + s[1:] + '.'
+
+
 help = {
-    'bracket': {'brief': 'print the bracket URL'},
-    'matches': {'brief': 'print the current matches'},
-    'noshow': {
-        'brief': 'start DQ process for player',
-        'usage': '@Player',
-        'description': (
-            'Notify @Player that they are in danger of being DQed. They have '
-            '5 minutes to post in the chat before being removed from bracket.')
-    },
-    'rename': {
-        'brief': 'rename player to their Discord tag',
-        'usage': 'TAG @Player',
-    },
-    'report': {
-        'brief': 'report a match',
-        'description': (
-            "Report a match result. The reporter's score goes first."),
-        'usage': '0-2 OR\n!auTO 0-2',
-    },
-    'results': {
-        'brief': 'print Top 8',
-        'description': 'Print Top 8 if the bracket is finished.',
-    },
-    'start': {
-        'brief': 'start running bracket',
-        'description': 'Start TOing and calling matches.',
-        'usage': 'CHALLONGE_URL'
-    },
-    'status': {'brief': 'how far along the tournament is'},
-    'stop': {'brief': 'stop TOing'},
-    'update_tags': {'brief': 'get latest Challonge tags'},
+    'bracket': HelpDoc('print the bracket URL'),
+    'matches': HelpDoc('print the current matches'),
+    'noshow': HelpDoc(
+        'start DQ process for player',
+        ('Notify @Player that they are in danger of being DQed. They have '
+         '5 minutes to post in the chat before being removed from bracket.'),
+        '@Player'),
+    'rename': HelpDoc(
+        'rename player to their Discord tag',
+        '',
+        '"TAG" @Player'),
+    'report': HelpDoc(
+        'report a match',
+        "Report a match result. The reporter's score goes first.",
+        '0-2 OR\n!auTO 0-2'),
+    'results': HelpDoc(
+        'print Top 8',
+        'Print Top 8 if the bracket is finished.'),
+    'start': HelpDoc(
+        'start running bracket',
+        'Start TOing and calling matches.',
+        'CHALLONGE_URL'),
+    'status': HelpDoc('how far along the tournament is'),
+    'stop': HelpDoc('stop TOing'),
+    'update_tags': HelpDoc('get latest Challonge tags'),
 }
-
-def descriptify(s):
-    return s[0].upper() + s[1:] + '.'
-
-for cmd, args in help.items():
-    if 'usage' not in args:
-        help[cmd]['usage'] = ''
-    if 'description' not in args:
-        help[cmd]['description'] = descriptify(help[cmd]['brief'])
